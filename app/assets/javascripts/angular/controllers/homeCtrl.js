2ca176d8efc47aac49f73ca81562e46d63093d79
Tomo Hung <tomohung@gmail.com>
2015-03-31 15:10:44 +0800

    backup

--- app/assets/javascripts/angular/controllers/homeCtrl.js
+++ app/assets/javascripts/angular/controllers/homeCtrl.js
@@ -2,5 +2,5 @@ angular.module('flapperNews')
 .controller('HomeCtrl', [
 '$scope',
 function($scope){
-  $scope.test = 'From AngularJs: This is home page.';
+  $scope.test = 'This is home page by AngularJS.';
 }]);
